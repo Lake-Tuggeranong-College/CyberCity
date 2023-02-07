35911c33a81468fbf8d9cfb309700fec1ca020d7
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-02-07 14:04:19 +1100

    Added positive message for good db connection

--- RC/config.php
+++ RC/config.php
@@ -5,12 +5,18 @@ $servername = "10.76.43.63";
 $username = "RC";
 $password = "RC";
 $dbname = "RC";
+$errorCaught = false;
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
+    $errorCaught = true;
     echo "Error: " . $e->getMessage();
+}
+
+if (!$errorCaught) {
+    echo "Database connection configured correctly, and database connection good.";
 }
 
 $conn = null;
