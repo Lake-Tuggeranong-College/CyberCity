0364a3888ab955a82f8f3485321d1bfd9766a5d7
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-02-14 14:21:18 +1100

    fixed sql

--- RC/login.php
+++ RC/login.php
@@ -1,24 +1,24 @@
 <?php include "template.php"; ?>
-    <title>Cyber City - Login</title>
+<title>Cyber City - Login</title>
 
-    <h1 class='text-primary'>Login</h1>
+<h1 class='text-primary'>Login</h1>
 
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
-        <div class="container-fluid">
-            <div class="row">
-                <!--Customer Details-->
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+    <div class="container-fluid">
+        <div class="row">
+            <!--Customer Details-->
 
-                <div class="col-md-12">
-                    <h2>Account Details</h2>
-                    <p>Please enter wanted username and password:</p>
-                    <p>User Name<input type="text" name="username" class="form-control" required="required"></p>
-                    <p>Password<input type="password" name="password" class="form-control" required="required"></p>
+            <div class="col-md-12">
+                <h2>Account Details</h2>
+                <p>Please enter wanted username and password:</p>
+                <p>User Name<input type="text" name="username" class="form-control" required="required"></p>
+                <p>Password<input type="password" name="password" class="form-control" required="required"></p>
 
-                </div>
             </div>
         </div>
-        <input type="submit" name="formSubmit" value="Submit">
-    </form>
+    </div>
+    <input type="submit" name="formSubmit" value="Submit">
+</form>
 
 
 <?php
@@ -27,16 +27,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = sanitise_data($_POST['username']);
     $password = sanitise_data($_POST['password']);
 
-    $query = $conn->query("SELECT COUNT(*) as count, * FROM user WHERE username='$username'");
+    $query = $conn->query("SELECT COUNT(*) as count FROM `user` WHERE `username`='$username'");
     $row = $query->fetch();
     $count = $row[0];
 
     if ($count > 0) {
-        if (password_verify($password, $row['hashed_password'])) {
-            $_SESSION["user_id"] = $row[1];
-            $_SESSION["name"] = $row[4];
-            $_SESSION["username"] = $row[2];
-            $_SESSION['level'] = $row[6];
+        $query = $conn->query("SELECT * FROM `user` WHERE `username`='$username'");
+        $row = $query->fetch();
+        if (password_verify($password, $row[2])) {
+            $_SESSION["user_id"] = $row[0];
+            $_SESSION["username"] = $row[1];
+            $_SESSION['access_level'] = $row[3];
             echo "successful";
 //            header("location:profile.php");
         } else {
@@ -44,9 +45,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 }
-
-?>
-
-
 
 ?>
