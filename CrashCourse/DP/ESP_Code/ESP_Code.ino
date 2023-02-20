b0c3ad8e3c8e9c9d6acea4cbc8ec293bdcd4d934
Dylan <dylanjp@iinet.net.au>
2023-02-20 15:27:41 +1100

    Update ESP_Code.ino

--- CrashCourse/DP/ESP_Code/ESP_Code.ino
+++ CrashCourse/DP/ESP_Code/ESP_Code.ino
@@ -1,9 +1,16 @@
 #include "config.h"
 
+#include <Wire.h>
+#include "Adafruit_ADT7410.h"
+Adafruit_ADT7410 tempsensor = Adafruit_ADT7410();
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  if (!tempsensor.begin()) {
+    Serial.println("Couldn't find ADT7410!");
+    logEvent("Couldn't find ADT7410!");
+    while (1);
+  }
 }
 
 void loop() {
