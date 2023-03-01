8eeb8a3e1b6e4ffd351d830a6ecbcabaab1b984f
novastick <99231858+novastick@users.noreply.github.com>
2023-03-02 10:08:32 +1100

    uploading data

--- CrashCourse/JR/esp-data.php
+++ CrashCourse/JR/esp-data.php
@@ -1,6 +1,4 @@
-<!DOCTYPE html>
-<html><body>
-<?php require_once 'config.php';
+<?php
 /*
   Rui Santos
   Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-mysql-database-php/
@@ -11,52 +9,53 @@
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
 */
-
+/** @var TYPE_NAME $conn */
+include "template.php";
 
 
 $sql = "SELECT id, sensor, location, value1, value2, value3, reading_time FROM SensorData ORDER BY id DESC";
 
-echo '<table cellspacing="5" cellpadding="5">
-      <tr> 
-        <td>ID</td> 
-        <td>Sensor</td> 
-        <td>Location</td> 
-        <td>Value 1</td> 
-        <td>Value 2</td>
-        <td>Value 3</td> 
-        <td>Timestamp</td> 
-      </tr>';
-
-if ($result = $conn->query($sql)) {
-    while ($row = $result->fetch_assoc()) {
-        $row_id = $row["id"];
-        $row_sensor = $row["sensor"];
-        $row_location = $row["location"];
-        $row_value1 = $row["value1"];
-        $row_value2 = $row["value2"];
-        $row_value3 = $row["value3"];
-        $row_reading_time = $row["reading_time"];
-        // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
-        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
-
-        // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
-        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
-
-        echo '<tr> 
-                <td>' . $row_id . '</td> 
-                <td>' . $row_sensor . '</td> 
-                <td>' . $row_location . '</td> 
-                <td>' . $row_value1 . '</td> 
-                <td>' . $row_value2 . '</td>
-                <td>' . $row_value3 . '</td> 
-                <td>' . $row_reading_time . '</td> 
-              </tr>';
-    }
-    $result->free();
-}
-
-$conn->close();
 ?>
-</table>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-1">ID</div>
+        <div class="col-md-1">Sensor</div>
+        <div class="col-md-2">Location</div>
+        <div class="col-md-2">Value 1</div>
+        <!--        <div class="col-md-2">Value 2</div>-->
+        <!--        <div class="col-md-2">Value 3</div>-->
+        <div class="col-md-2">Timestamp</div>
+    </div>
+
+
+    <?php
+
+    if ($result = $conn->query($sql)) {
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $row_id = $row["id"];
+            $row_sensor = $row["sensor"];
+            $row_location = $row["location"];
+            $row_value1 = $row["value1"];
+//            $row_value2 = $row["value2"];
+//            $row_value3 = $row["value3"];
+            $row_reading_time = $row["reading_time"];
+            echo '
+    <div class="row">
+        <div class="col-md-1">' . $row_id . '</div>
+        <div class="col-md-1">' . $row_sensor . '</div>
+        <div class="col-md-2">' . $row_location . '</div>
+        <div class="col-md-2">' . $row_value1 . '</div>
+
+        <div class="col-md-2">' . $row_reading_time . '</div>
+    </div>
+';
+        }
+        // $result->free();
+        $result = null;
+    }
+
+    //$conn->close();
+    ?>
+</div>
 </body>
 </html>
