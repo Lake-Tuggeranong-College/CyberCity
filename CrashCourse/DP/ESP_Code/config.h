f152bbe5f1b9d89cf828b995bd4d171a7798754b
Dylan <dylanjp@iinet.net.au>
2023-03-02 10:27:19 +1100

    ESP posting works!

--- CrashCourse/DP/ESP_Code/config.h
+++ CrashCourse/DP/ESP_Code/config.h
@@ -1,13 +1,13 @@
 // Replace with your network credentials
-const char* ssid     = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* ssid     = "RoboRange";
+const char* password = "Password01";
 
 // REPLACE with your Domain name and URL path or IP address with path
-const char* serverName = "http://example.com/post-esp-data.php";
+const char* serverName = "http://192.168.1.8/CyberCity/CrashCourse/RC/post-esp-data.php";
 
-// Keep this API Key value to be compatible with the PHP code provided in the project page. 
-// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key 
+// Keep this API Key value to be compatible with the PHP code provided in the project page.
+// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key
 String apiKeyValue = "tPmAT5Ab3j7F9";
 
-String sensorName = "BME280";
-String sensorLocation = "Office";
+String sensorName = "Tempature";
+String sensorLocation = "Dylan";
