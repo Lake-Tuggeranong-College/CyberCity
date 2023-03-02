ebd98ac275cc161805dbac639043279dd66cebd1
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-03-02 11:25:28 +1100

    config to new system

--- CrashCourse/RC/esp-data.php
+++ CrashCourse/RC/esp-data.php
@@ -14,7 +14,7 @@
 include "template.php";
 
 
-$sql = "SELECT id, sensor, location, value1, value2, value3, reading_time FROM SensorData ORDER BY id DESC";
+$sql = "SELECT id, sensor, location, value1, reading_time FROM SensorData ORDER BY id DESC";
 
 ?>
 <div class="container-fluid">
@@ -53,6 +53,8 @@ $sql = "SELECT id, sensor, location, value1, value2, value3, reading_time FROM S
         }
         // $result->free();
         $result = null;
+    } else {
+        echo "Error in SQL";
     }
 
     //$conn->close();
