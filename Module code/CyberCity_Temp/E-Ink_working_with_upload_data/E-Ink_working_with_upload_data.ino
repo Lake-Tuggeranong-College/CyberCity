6066eae94e7bc6ef556904f6772b3b6e3d7e1b6d
0676662 <99225477+0676662@users.noreply.github.com>
2023-03-16 10:33:11 +1100

    Working temp display

---
+++ Module code/CyberCity_Temp/E-Ink_working_with_upload_data/E-Ink_working_with_upload_data.ino
@@ -0,0 +1,240 @@
+
+
+/***************************************************
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+
+
+// Wifi & Webserver
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <HTTPClient.h>
+#else
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+  #include <WiFiClient.h>
+#endif
+#include "sensitiveInformation.h"
+
+
+// RTC
+#include "RTClib.h"
+
+RTC_PCF8523 rtc;
+
+// EINK
+#include "Adafruit_ThinkInk.h"
+
+#define EPD_CS      15
+#define EPD_DC      33
+#define SRAM_CS     32
+#define EPD_RESET   -1 // can set to -1 and share with microcontroller Reset!
+#define EPD_BUSY    -1 // can set to -1 to not use a pin (will wait a fixed delay)
+
+// 2.13" Monochrome displays with 250x122 pixels and SSD1675 chipset
+ThinkInk_213_Mono_B72 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+const char* serverName = "http://192.168.1.18/CyberCity/CrashCourse/RC/post-esp-data.php";
+
+// Keep this API Key value to be compatible with the PHP code provided in the project page. 
+// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key 
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+String sensorName = "FireStationTemp";
+String sensorLocation = "FireStation";
+
+//Temperature Sensor
+#include <Wire.h>
+#include "Adafruit_ADT7410.h"
+// Create the ADT7410 temperature sensor object
+Adafruit_ADT7410 tempsensor = Adafruit_ADT7410();
+
+void setup() {
+  /*
+  */
+  Serial.begin(9600);
+  while (!Serial) {
+    delay(10);
+  }
+  delay(1000);
+
+
+  if (!tempsensor.begin()) {
+    Serial.println("Couldn't find ADT7410!");
+    while (1);
+  }
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println();
+  Serial.print("Connected to the Internet");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  
+
+
+  // RTC
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    //    abort();
+  }
+
+  // The following line can be uncommented if the time needs to be reset.
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  rtc.start();
+
+  //EINK
+  display.begin();
+  display.clearBuffer();
+
+  
+  logEvent("System Initialisation...");
+}
+
+
+void loop() {
+  updateEPD("Fire Dept", "Temp \tC", tempsensor.readTempC());
+
+
+  // waits 180 seconds (3 minutes) as per guidelines from adafruit.
+  delay(180000);
+  display.clearBuffer();
+   //Check WiFi connection status
+  if(WiFi.status()== WL_CONNECTED){
+    WiFiClient client;
+    HTTPClient http;
+    
+    // Your Domain name with URL path or IP address with path
+    http.begin(client, serverName);
+    
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+    // Prepare your HTTP POST request data
+    String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
+                          + "&location=" + sensorLocation + "&value1=" + "&value3=" + String(tempsensor.readTempC()) + "";
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+    
+    // You can comment the httpRequestData variable above
+    // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
+    // String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&location=Office&value1=24.75&value2=49.54&value3=1005.14";
+
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(httpRequestData);
+     
+    // If you need an HTTP request with a content type: text/plain
+    //http.addHeader("Content-Type", "text/plain");
+    // int httpResponseCode = http.POST("Hello, World!");
+    
+    // If you need an HTTP request with a content type: application/json, use the following:
+    // http.addHeader("Content-Type", "application/json");
+    // int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
+    
+
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  //Send an HTTP POST request every 30 seconds
+  delay(30000);  
+}
+
+
+void updateEPD(String title, String dataTitle, float dataToDisplay) {
+
+  // Indigenous Country Name
+  drawText(title, EPD_BLACK, 2, 0, 0);
+
+  // Config
+  drawText(WiFi.localIP().toString(), EPD_BLACK, 1, 130, 80);
+  drawText(getTimeAsString(), EPD_BLACK, 1, 130, 100);
+  drawText(getDateAsString(), EPD_BLACK, 1, 130, 110);
+
+  // Draw lines to divvy up the EPD
+  display.drawLine(0, 20, 250, 20, EPD_BLACK);
+  display.drawLine(125, 20, 125, 122, EPD_BLACK);
+  display.drawLine(0, 75, 250, 75, EPD_BLACK);
+
+  drawText(dataTitle, EPD_BLACK, 2, 0, 80);
+  drawText(String(dataToDisplay), EPD_BLACK, 4, 0, 95);
+
+  logEvent("Updating the EPD");
+  display.display();
+
+}
+
+
+
+void drawText(String text, uint16_t color, int textSize, int x, int y) {
+  display.setCursor(x, y);
+  display.setTextColor(color);
+  display.setTextSize(textSize);
+  display.setTextWrap(true);
+  display.print(text);
+}
+
+String getDateAsString() {
+  DateTime now = rtc.now();
+
+  // Converts the date into a human-readable format.
+  char humanReadableDate[20];
+  sprintf(humanReadableDate, "%02d/%02d/%02d", now.day(), now.month(), now.year());
+
+  return humanReadableDate;
+}
+
+String getTimeAsString() {
+  DateTime now = rtc.now();
+
+  // Converts the time into a human-readable format.
+  char humanReadableTime[20];
+  sprintf(humanReadableTime, "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
+
+  return humanReadableTime;
+}
+
+void logEvent(String dataToLog) {
+  /*
+     Log entries to a file stored in SPIFFS partition on the ESP32.
+  */
+  // Get the updated/current time
+  DateTime rightNow = rtc.now();
+  char csvReadableDate[25];
+  sprintf(csvReadableDate, "%02d,%02d,%02d,%02d,%02d,%02d,",  rightNow.year(), rightNow.month(), rightNow.day(), rightNow.hour(), rightNow.minute(), rightNow.second());
+
+  String logTemp = csvReadableDate + dataToLog + "\n"; // Add the data to log onto the end of the date/time
+
+  const char * logEntry = logTemp.c_str(); //convert the logtemp to a char * variable
+
+  //Add the log entry to the end of logevents.csv
+
+  // Output the logEvents - FOR DEBUG ONLY. Comment out to avoid spamming the serial monitor.
+  //  readFile(SPIFFS, "/logEvents.csv");
+
+  Serial.print("\nEvent Logged: ");
+  Serial.println(logEntry);
+}
