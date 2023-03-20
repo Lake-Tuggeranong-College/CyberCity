f988535cfa51dc03930849e156867fd841ca1a5d
0676662 <0676662@schoolsnet.act.edu.au>
2023-03-20 15:27:48 +1100

    newly updated with working code and esp32 post code
    newly updated with working code and esp32 post code

--- Module code/FeatherWingTest/FeatherWingTest.ino
+++ Module code/FeatherWingTest/FeatherWingTest.ino
@@ -13,6 +13,7 @@
 // Wifi & Webserver
 #include "WiFi.h"
 #include "sensitiveInformation.h"
+#include <HTTPClient.h>
 
 // RTC
 #include "RTClib.h"
@@ -66,7 +67,7 @@ void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);
 
-  
+
 
 
   // RTC
@@ -85,7 +86,7 @@ void setup() {
   display.begin();
   display.clearBuffer();
 
-  
+
   logEvent("System Initialisation...");
 }
 
@@ -98,6 +99,60 @@ void loop() {
   display.clearBuffer();
 
 }
+void uploadData () {
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient client;
+    HTTPClient http;
+
+    // Your Domain name with URL path or IP address with path
+    http.begin(client, serverName);
+
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // Prepare your HTTP POST request data
+    String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
+                             + "&location=" + sensorLocation + "&value1=" + String(tempsensor.readTempC()) + "";
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+
+    // You can comment the httpRequestData variable above
+    // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
+    // String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&location=Office&value1=24.75&value2=49.54&value3=1005.14";
+
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(httpRequestData);
+
+    // If you need an HTTP request with a content type: text/plain
+    //http.addHeader("Content-Type", "text/plain");
+    // int httpResponseCode = http.POST("Hello, World!");
+
+    // If you need an HTTP request with a content type: application/json, use the following:
+    // http.addHeader("Content-Type", "application/json");
+    // int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
+
+
+    if (httpResponseCode > 0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  //Send an HTTP POST request every 30 seconds
+  delay(30000);
+
+}
+
+
+
 
 void updateEPD(String title, String dataTitle, float dataToDisplay) {
 
@@ -121,8 +176,6 @@ void updateEPD(String title, String dataTitle, float dataToDisplay) {
   display.display();
 
 }
-
-
 
 void drawText(String text, uint16_t color, int textSize, int x, int y) {
   display.setCursor(x, y);
