dd0b47f388951e081c7d795bb8c392d48838b94e
Charlotte.b <0342544@schoolsnet.act.edu.au>
2023-03-28 11:56:16 +1100

    traffic light code1.0

---
+++ Module code/traficTest1.0/traficTest1.0.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(23,OUTPUT);
+  pinMode(32,OUTPUT);
+  pinMode(14,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(14,HIGH);
+  digitalWrite(23,HIGH);
+  digitalWrite(32,HIGH);
+}
