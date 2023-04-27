15e9f4e504529ef1209fccab00b306d858c84e19
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-04-27 10:59:03 +1000

    init output correct for module info. Update array referencing.

--- moduleInformation.php
+++ moduleInformation.php
@@ -13,24 +13,34 @@ if (isset($_GET["ModuleID"])) {
     header("location:moduleList.php");
 }
 
-$sql= $conn->query("SELECT ID, Location, Module FROM RegisteredModules WHERE ID= .$moduleToLoad. ");
+$sql = $conn->query("SELECT ID, Location, Module, CurrentOutput FROM RegisteredModules WHERE ID= " . $moduleToLoad);
 $moduleInformation = $sql->fetch();
-    $moduleID = $moduleInformation[0];
-    $moduleLocation = $moduleInformation[1];
-    $moduleName = $moduleInformation[2];
+$moduleID = $moduleInformation["ID"];
+$moduleLocation = $moduleInformation["Location"];
+$moduleName = $moduleInformation["Module"];
+$moduleOutput = $moduleInformation["CurrentOutput"];
 
 
-
-
+echo 'Debug Information. Comment out as necessary<pre>';
+print_r($moduleInformation);
+echo '</pre>';
 
 ?>
 
+<h1 class='text-primary'>Module Name <?php echo $moduleName; ?></h1>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-6">
-            <h3>Module Name <?php echo $moduleName; ?></h3>
+        <div class="col-md-4">
+            <?= $moduleName ?>
         </div>
-
+        <div class="col-md-4">
+            <?= $moduleLocation ?>
+        </div>
+        <div class="col-md-4">
+            <?= $moduleOutput ?>
+        </div>
+    </div>
+</div>
 
 
 
