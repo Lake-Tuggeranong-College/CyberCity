6a92d23049cb5775c19d17ff5f36b4a44877a233
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-04-27 13:51:30 +1000

    fixed navbar

--- template.php
+++ template.php
@@ -40,7 +40,7 @@
                         <a class="dropdown-item" href="userSearch.php">User Search</a>
 
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="moduleRegister.php">Add Products</a>
+                        <a class="dropdown-item" href="moduleRegister.php">Add New Module</a>
                         <?php
                     }
                     ?>
