bb8c67aca0f91f8356d2ff6f9b4e22297b3e3b99
GodlyBlueDog <99225553+GodlyBlueDog@users.noreply.github.com>
2023-05-01 15:33:07 +1000

    Update moduleDisplay.php

--- moduleDisplay.php
+++ moduleDisplay.php
@@ -1,26 +1,41 @@
 <?php include "template.php";
 /** @var $conn */ ?>
 
+<!DOCTYPE html>
+<html>
+<head>
 
 </head>
+<body>
 
-<?php
-if (isset($_GET["ModuleID"])) {
-    $moduleToLoad = $_GET["ModuleID"];
-} else {
+
+<div class="table-responsive">
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+
+            <th>Date & Time</th>
+            <th>Data </th>
+
+        </thead>
+</body>
+</html>
+
+        <?php
+        if (isset($_GET["ModuleID"])) {
+            $moduleToLoad = $_GET["ModuleID"];
+        } else {
     header("location:moduleList.php");
 }
 
-?>
+        $sql = "SELECT EventID, ModuleID, DateTime, Data FROM ModuleData WHERE ModuleID = ".$moduleToLoad." ORDER BY EventID DESC";
 
-<body>
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-6">Date & Time</div>
-        <div class="col-6">Data</div>
-
-    </div>
+        if ($result = $conn->query($sql)) {
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                $row_id = $row["EventID"];
+                $row_moduleID = $row["ModuleID"];
+                $row_dateTime = $row["DateTime"];
+                $row_data = $row["Data"];
 
    echo "<title> Module ID:" .$row_moduleID.  "</title>";
                 echo "<tr>";
@@ -29,9 +44,9 @@ if (isset($_GET["ModuleID"])) {
                 echo "<td>" . $row_data .  "  </td>";
                 echo "</tr>";
 
+            }
+            $result = null;
         }
-        $result = null;
-    }
-    ?>
-</div>
+        ?>
+
 <?php echo outputFooter(); ?>
