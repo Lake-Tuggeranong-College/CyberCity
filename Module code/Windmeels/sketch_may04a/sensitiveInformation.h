3f7e782b5a43d279828318ab1a46b45f758a207b
0676662 <0676662@schoolsnet.act.edu.au>
2023-05-04 10:27:14 +1000

    api key fix - train timer and servo

--- Module code/Windmeels/sketch_may04a/sensitiveInformation.h
+++ Module code/Windmeels/sketch_may04a/sensitiveInformation.h
@@ -5,7 +5,7 @@ const char* password = "Password01";  // Wifi Password
 //const char* serverName = "http://192.168.1.106/espPost/post-esp-data.php";
 String serverName = "http://192.168.1.18/postESPData.php";
 
-String apiKeyValue = "1";
+String apiKeyValue = "lqJPAy";
 
-String sensorName = "Windmill";
+String sensorName = "Sevro";
 String sensorLocation = "Windmill";
