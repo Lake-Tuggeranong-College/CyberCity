2f96664ef764b591d9e2fea8f0d86f1e7701b31d
0676662 <0676662@schoolsnet.act.edu.au>
2023-05-04 10:24:12 +1000

    windmeel working :)

---
+++ Module code/Windmeels/sketch_may04a/sensitiveInformation.h
@@ -0,0 +1,11 @@
+const char* host = "RMS";
+const char* ssid = "RoboRange";       // Wifi Network Name
+const char* password = "Password01";  // Wifi Password
+
+//const char* serverName = "http://192.168.1.106/espPost/post-esp-data.php";
+String serverName = "http://192.168.1.18/postESPData.php";
+
+String apiKeyValue = "1";
+
+String sensorName = "Windmill";
+String sensorLocation = "Windmill";
