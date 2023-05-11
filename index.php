aefd0998fc7506223371b239c846f9829e74b76f
Deltace05 <ben.mackenzie05@gmail.com>
2023-05-11 10:09:32 +1000

    added image support to index & module list

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 
 
 <title>Cyber City</title>
-<h1 class='text-primary'>Welcome to our The Cyber City</h1>
+<h1 class='text-primary'>Welcome to our Cyber City</h1>
+<img src="images/MainImage.jpg" alt="" width="50%" height="50%">
+<p></p>
 <?php echo outputFooter(); ?>
 </body>
 </html>
