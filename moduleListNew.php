8c8667841d5b0be038ceec13ce03d4b6b8a52c84
Saxonugfd7 <99225592+Saxonugfd7@users.noreply.github.com>
2023-05-15 15:40:09 +1000

    Started making new moudlue list

---
+++ moduleListNew.php
@@ -0,0 +1,31 @@
+<?php include "template.php";
+/** @var $conn */ ?>
+
+
+<title>Module List</title>
+    <link rel="stylesheet" href="css/moduleList.css">
+
+<h1 class='text-primary'>Module List</h1>
+
+<?php
+$moduleList = $conn->query("SELECT Location, Module, ID, Enabled, Image FROM RegisteredModules");
+?>
+
+<?php
+
+    while ($moduleData = $moduleList->fetch() ) {
+        $moduleID = $moduleData["ID"];
+        echo "<div class='product_wrapper'>
+        <div class='image'><img src='images/modules/" . $moduleData['Image'] . "' width='100' height='100'/></div>
+        <div class='name'>" . $moduleData[0] . "</div>
+        <div class='price'> $moduleData[1]</div>
+        ";
+        if ($_SESSION["access_level"] == 2) {
+            echo "moduleInformation.php?ModuleID=" . $moduleID;
+            echo"
+                <button href='moduleInformation.php?ModuleID=<?= $moduleID ?>'>Information</button>
+                <button href='moduleEdit.php?ModuleID=<?= $moduleID ?>'>Edit</button>
+    ";
+    echo"</div>";
+        }
+    }
