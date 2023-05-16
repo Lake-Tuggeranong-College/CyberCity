bf1ff7888975cd6f25fb3a4580ab5d0a96fbfc0f
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-05-16 14:40:24 +1000

    added link to image. Fixed buggy div

--- moduleList.php
+++ moduleList.php
@@ -2,31 +2,29 @@
 /** @var $conn */ ?>
 
 
-<title>Module List</title>
+    <title>Module List</title>
     <link rel="stylesheet" href="css/moduleList.css">
 
-<h1 class='text-primary'>Module List</h1>
+    <h1 class='text-primary'>Module List</h1>
 
 <?php
 $moduleList = $conn->query("SELECT Location, Module, ID, Enabled, Image FROM RegisteredModules WHERE Enabled=1"); #Get all Enabled Modules
-    while ($moduleData = $moduleList->fetch() ) {
-        $moduleID = $moduleData["ID"];
-        echo "<div class='product_wrapper'>";
-            if ($moduleData['Image']) { #Does the Module have an Image?
-                echo "<div class='image'><img src='images/modules/" . $moduleData['Image'] . "' width='100' height='100'/></div>"; #Display Module Image
-            }
-    else {
-        echo "<div class='image'><img src='images/modules/blank.jpg'width='100' height='100'/></div>"; #Display Placeholder Image
+while ($moduleData = $moduleList->fetch()) {
+    $moduleID = $moduleData["ID"];
+    echo "<div class='product_wrapper'>";
+    if ($moduleData['Image']) { #Does the Module have an Image?
+        echo "<div class='image'><a href='moduleDisplay.php?ModuleID=".$moduleID."'><img src='images/modules/" . $moduleData['Image'] . "' width='100' height='100'/></a></div>"; #Display Module Image
+    } else {
+        echo "<div class='image'><a href='moduleDisplay.php?ModuleID=".$moduleID."'><img src='images/modules/blank.jpg'width='100' height='100'/></a></div>"; #Display Placeholder Image
     }
- echo"
+    echo "
         <div class='name'>" . $moduleData[0] . "</div>
         <div class='price'> $moduleData[1]</div>
         ";
-if ($_SESSION["access_level"] == 2) {
-    echo"
+    if ($_SESSION["access_level"] == 2) {
+        echo "
                 <a class='moduleButton' href='moduleInformation.php?ModuleID=" . $moduleID . "'>Information</a>
-                <a class='moduleButton' href='moduleEdit.php?ModuleID=" . $moduleID . "'>Edit</a>
- 
-    </div>";
-        }
+                <a class='moduleButton' href='moduleEdit.php?ModuleID=" . $moduleID . "'>Edit</a>";
     }
+    echo "</div>";
+}
