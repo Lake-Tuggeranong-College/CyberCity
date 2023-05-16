f0ddd32bafd261134c00523267168efc3880e266
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-05-16 13:48:07 +1000

    http get output

--- dataTransfer.php
+++ dataTransfer.php
@@ -48,5 +48,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     }
 } else {
-echo "test";
+$api_key = sanitise_data($_POST["api_key"]);
+    $location = sanitise_data($_POST["location"]);
+
+    $query = $conn->query("SELECT COUNT(*) as count FROM `RegisteredModules` WHERE `Location` ='$location'");
+    $row = $query->fetch();
+    $count = $row[0];
+    if ($count > 0) {
+        $query = $conn->query("SELECT * FROM `RegisteredModules` WHERE `Location`='$location'");
+        $row = $query->fetch();
+        $payload = $row[4];
+        $api_key_value = $row[3];
+        if (password_verify($api_key, $api_key_value)) {
+            $sensorValue = sanitise_data($_POST["sensorValue"]);
+            date_default_timezone_set('Australia/Canberra');
+            $date = date("Y-m-d H:i:s");
+            //DO NOT CHANGE THIS DATE CODE, MUST STAY SAME TO WORK WITH MYSQL
+            $ModuleID = $row[0];
+//            $query = $conn->query("SELECT CurrentOutput from RegisteredModules where Location=$location");
+//            $row = $query->fetch();
+            echo "Payload:".$payload;
+            $conn->close();
+        } else {
+
+        }
+    } else {
+
+    }
 }
