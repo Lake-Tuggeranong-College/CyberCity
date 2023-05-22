fecfcfdfc25ac73715f915f008f9e7150efe993e
novastick <0157850@schoolsnet.act.edu.au>
2023-05-22 15:25:49 +1000

    traffic lights working

--- Module code/TraficLights/sensitiveInformation.h
+++ Module code/TraficLights/sensitiveInformation.h
@@ -13,7 +13,8 @@ const char* ssid            = "RoboRange";        // Wifi Network Name
 const char* password        = "Password01";       // Wifi Password
 
 //const char* serverName = "http://192.168.1.106/espPost/post-esp-data.php";
- const char* serverName = "http://192.168.1.18/postESPData.php";
+ //const char* serverName = "http://192.168.1.18/postESPData.php";
+ String serverName = "http://192.168.1.18/dataTransfer.php";
 
 String apiKeyValue = "IcFxFF";
 
