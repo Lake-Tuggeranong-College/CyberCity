89c668cd34803b6c01887a925de9b26dd24f223a
Dylan <dylanjp@iinet.net.au>
2023-08-08 11:54:32 +1000

    Started work on challenge list

--- template.php
+++ template.php
@@ -22,7 +22,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="moduleList.php">Modules</a></li>
+                <li class="nav-item"><a class="nav-link" href="challengesList.php">Challenges</a></li>
                 <li class="nav-item"><a class="nav-link" href="leaderboard.php">Leaderboard</a></li>
                 <li class="nav-item"><a class="nav-link" href="contact.php">Contact Us</a></li>
 
