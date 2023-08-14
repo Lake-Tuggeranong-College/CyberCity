735313ae688554b47ce9fd3afc64e47ba3ef3426
Dylan <dylanjp@iinet.net.au>
2023-08-14 15:29:09 +1000

    Update moduleRegister.php

--- moduleRegister.php
+++ moduleRegister.php
@@ -35,56 +35,60 @@ if (!authorisedAccess(false, false, true)) {
 
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") { //Make Module Entry
-    $location = ($_POST['Location']);
-    $module = ($_POST['Module']);
-    $APIkey = ($_POST['APIkey']);
-
-
-    $hashed_APIkey = password_hash($APIkey, PASSWORD_DEFAULT);
-    //echo  $location;
-    //echo  $module;
-    //echo  $hashed_APIkey;
-
-// check Module and location in database
-    $query = $conn->query("SELECT COUNT(*) FROM `RegisteredModules` WHERE Location='$location'");
-    $query2 = $conn->query("SELECT COUNT(*) FROM `RegisteredModules` WHERE Module='$module'");
-    $data = $query->fetch();
-    $data2 = $query2->fetch();
-    $checkModule = (int)$data[0];
-    $checkLocation = (int)$data2[0];
-
-    if ($checkModule > 0 && $checkLocation > 0) {
-        echo "This Module is already in use at this location";
-    } else {
-        $sql = "INSERT INTO `RegisteredModules` (Location, Module, HashedAPIKey, Enabled) VALUES (:newLocation, :newModule, :newHashedAPIkey, :enabled)";
-
-        $stmt = $conn->prepare($sql);
-        $stmt->bindValue(':newLocation', $location);
-        $stmt->bindValue(':newModule', $module);
-        $stmt->bindValue(':newHashedAPIkey', $hashed_APIkey);
-        $stmt->bindValue(':enabled', 1);
-        $stmt->execute();
-        //$_SESSION["flash_message"] = "Module Created";
-        //header("Location:index.php");
-    }
-
-}
+//if ($_SERVER["REQUEST_METHOD"] == "POST") { //Make Module Entry
+//    $location = ($_POST['Location']);
+//    $module = ($_POST['Module']);
+//    $APIkey = ($_POST['APIkey']);
+//
+//
+//    $hashed_APIkey = password_hash($APIkey, PASSWORD_DEFAULT);
+//    //echo  $location;
+//    //echo  $module;
+//    //echo  $hashed_APIkey;
+//
+//// check Module and location in database
+//    $query = $conn->query("SELECT COUNT(*) FROM `RegisteredModules` WHERE Location='$location'");
+//    $query2 = $conn->query("SELECT COUNT(*) FROM `RegisteredModules` WHERE Module='$module'");
+//    $data = $query->fetch();
+//    $data2 = $query2->fetch();
+//    $checkModule = (int)$data[0];
+//    $checkLocation = (int)$data2[0];
+//
+//    if ($checkModule > 0 && $checkLocation > 0) {
+//        echo "This Module is already in use at this location";
+//    } else {
+//        $sql = "INSERT INTO `RegisteredModules` (Location, Module, HashedAPIKey, Enabled) VALUES (:newLocation, :newModule, :newHashedAPIkey, :enabled)";
+//
+//        $stmt = $conn->prepare($sql);
+//        $stmt->bindValue(':newLocation', $location);
+//        $stmt->bindValue(':newModule', $module);
+//        $stmt->bindValue(':newHashedAPIkey', $hashed_APIkey);
+//        $stmt->bindValue(':enabled', 1);
+//        $stmt->execute();
+//        //$_SESSION["flash_message"] = "Module Created";
+//        //header("Location:index.php");
+//    }
+//
+//}
 if ($_SERVER["REQUEST_METHOD"] == "POST") { //Make Challange Entry
     $flag = sanitise_data($_POST['flag']);
     $hashed_flag = password_hash($flag, PASSWORD_DEFAULT);
     $points = sanitise_data($_POST['pointsValue']);
     $title = sanitise_data($_POST['challengeTitle']);
     $disc = sanitise_data($_POST['challengeDescription']);
-    $flagList = $conn->query("SELECT HashedFlag FROM Challenges");
-    $sql = "INSERT INTO Challanges (HashedFlag, PointsValue,challangeTitle,challangeText,moduleID) VALUES (:newFlag, :newPoints,:newTitle,:newText)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindValue(':newFlag', $hashed_flag);
-    $stmt->bindValue(':newPoints', $points);
-    $stmt->bindValue(':newTitle', $title);
-    $stmt->bindValue(':newText', $disc);
-    $stmt->execute();
-    echo "Flag Made";
+    echo "Got POST data";
+//    $flagList = $conn->query("SELECT HashedFlag FROM Challenges");
+    $sql2 = "INSERT INTO `Challanges` (HashedFlag, PointsValue, challangeTitle, challangeText) VALUES (:newFlag, :newPoints, :newTitle, :newText)";
+    echo "made SQL";
+    $stmt2 = $conn->prepare($sql2);
+    echo "made stmt";
+    $stmt2->bindValue(':newFlag', $hashed_flag);
+    $stmt2->bindValue(':newPoints', $points);
+    $stmt2->bindValue(':newTitle', $title);
+    $stmt2->bindValue(':newText', $disc);
+    echo "bind stmt";
+    $stmt2->execute();
+    echo "execute";
 
 }
 ?>
