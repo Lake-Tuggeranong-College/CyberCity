6a1d770fff064d3b0a6e61e34c4af1319c44c832
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-08-21 21:58:06 +1000

    fix link/s

--- userList.php
+++ userList.php
@@ -7,27 +7,18 @@ if (!authorisedAccess(false, false, true)) {
 
 ?>
 
-    <title>userList</title>
+    <title>User List</title>
     <link rel="stylesheet" href="css/moduleList.css">
 
-    <h1 class='text-primary'>userList</h1>
+    <h1 class='text-primary'>User List</h1>
 
 <?php
 $userList = $conn->query("SELECT ID, Username, AccessLevel, Enabled FROM Users WHERE Enabled=1"); #Get all Enabled Modules
 while ($userData = $userList->fetch()) {
     $userID = $userData["ID"];
     echo "<div class='product_wrapper'>";
-    echo "
-        <div class='name'>" . $userData["Username"] . "</div>
-        <div class='price'>". $userData["AccessLevel"]."</div>
-        ";
-    if ($_SESSION["access_level"] == 2) {
-        echo "
-                <a class='moduleButton' href='moduleInformation.php?ModuleID=" . $userID . "'>Information</a>
-                <a class='moduleButton' href='moduleEdit.php?ModuleID=" . $userID . "'>Edit</a>";
-    }
+    echo "<a class='moduleButton' href='userEdit.php?ModuleID=" . $userID . "'>Edit - " . $userData["Username"] . "</a>";
     echo "</div>";
-
 }
 
 
@@ -37,7 +28,7 @@ while ($userData = $userList->fetch()) {
     echo "<div class='product_wrapper'>";
     echo "
         <div class='name'>" . $userData["Username"] . "</div>
-        <div class='price'>". $userData["AccessLevel"]."</div>
+        <div class='price'>" . $userData["AccessLevel"] . "</div>
         ";
     if ($_SESSION["access_level"] == 2) {
         echo "
