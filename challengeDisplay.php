44b8785615ba27d74df6d8a4c88d528c1bf79b71
GodlyBlueDog <99225553+GodlyBlueDog@users.noreply.github.com>
2023-08-22 11:55:27 +1000

    Update challengeDisplay.php

--- challengeDisplay.php
+++ challengeDisplay.php
@@ -31,7 +31,7 @@ if (!authorisedAccess(false, true, true)) {
 if (isset($_GET["moduleID"])) {
     $challengeToLoad = $_GET["moduleID"];
 } else {
-    header("location:challengesList.php");
+   header("location:challengesList.php");
 }
 
 $sql = "SELECT moduleID, challengeTitle, challengeText, PointsValue FROM Challenges WHERE moduleID = " . $challengeToLoad . " ORDER BY ID DESC";
@@ -45,7 +45,6 @@ if ($result = $conn->query($sql)) {
 
         echo "<title> ID:" . $row_id . "</title>";
         echo "<tr>";
-
         echo "<td>" . $row_challengeTitle . "</td>";
         echo "<td>" . $row_challengeText . "  </td>";
         echo "<td>" . $row_PointsValue . "  </td>";
@@ -53,6 +52,28 @@ if ($result = $conn->query($sql)) {
 
     }
     $result = null;
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $flag = sanitise_data($_POST['flag']);
+    $challengeToLoad = $_GET["moduleID"];
+    $flagList = $conn->query("SELECT HashedFlag, PointsValue, moduleID, challengeTitle, challengeText, PointsValue FROM Challenges WHERE moduleID = " . $challengeToLoad . "");
+
+    while ($flagData = $flagList->fetch()) {
+        if (password_verify($flag, $flagData[0])) {
+            $username = $_SESSION["username"];
+
+            $userInformation = $conn->query("SELECT Username, Score FROM Users WHERE Username='$username'");
+            $userData = $userInformation->fetch();
+            $addedScore = $userData[1] += $flagData[1];
+            $sql1 = "UPDATE Users SET Score=? WHERE Username=?";
+            // change to UPDATE
+            $stmt = $conn->prepare($sql1);
+            $stmt->execute([$addedScore, $username]);
+            echo "added points";
+        } else {
+            echo "Could not find flag :(";
+        }
+    }
 }
 ?>
 
@@ -69,37 +90,7 @@ if ($result = $conn->query($sql)) {
 </body>
 </html>
 
-<?php
-/*
-if (isset($_GET["moduleID"])) {
-    $challengeToLoad = $_GET["moduleID"];
-} else {
-    header("location:moduleList.php");
-}*/
-//if (isset($_POST['login'])) {
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $flag = sanitise_data($_POST['flag']);
 
-    $flagList = $conn->query("SELECT HashedFlag,PointsValue FROM Challenges");
-
-    while ($flagData = $flagList->fetch()) {
-        if (password_verify($flag, $flagData[0])) {
-            $username = $_SESSION["username"];
-            $userInformation = $conn->query("SELECT Username, Score FROM Users WHERE Username='$username'");
-            $userData = $userInformation->fetch();
-            $addedScore = $userData[1] += $flagData[1];
-            $sql = "UPDATE Users SET Score=? WHERE Username=?";
-            // change to UPDATE
-            $stmt = $conn->prepare($sql);
-            $stmt->execute([$addedScore, $username]);
-            echo "added points";
-        } else {
-            echo "Could not find flag :(";
-        }
-    }
-}
-
-?>
 
 
 
