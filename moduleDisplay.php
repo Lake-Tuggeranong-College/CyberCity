cf8293a0a1f3e60ae427af96fc8f6fc913290030
GodlyBlueDog <99225553+GodlyBlueDog@users.noreply.github.com>
2023-08-31 10:27:29 +1000

    Replaced tables with div columns to support users who require the use of screen readers

--- moduleDisplay.php
+++ moduleDisplay.php
@@ -17,16 +17,34 @@ if (!authorisedAccess(false, true, true)) {
             <th>Data</th>
 
         </thead>-->
-<title>moduleDisplay</title>
-<link rel="stylesheet" href="css/divContainer.css">
-<div class="container">
+
+<html>
+<head>
+    <style>
+        #myDiv {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+
+
+<h1>Module - <?= $row_moduleID ?></h1>
+<div class="container-fluid">
     <div class="row">
-        <div class="cell">This is a table cell</div>
+        <div class="col-1 border border border-dark">Module ID</div>
+        <div class="col-1 border border border-dark">Module data</div>
+        <div class="col-2 border border border-dark">Date & Time</div>
+        <div class="col-1 border border border-dark">Event ID</div>
+
     </div>
+
+
 </div>
 
 
-
+</body>
+</html>
 
 
         <?php
@@ -45,20 +63,30 @@ if (!authorisedAccess(false, true, true)) {
                 $row_dateTime = $row["DateTime"];
                 $row_data = $row["Data"];
 
-                echo "<title> Module ID:" . $row_moduleID . "</title>";
+               /* echo "<title> Module ID:" . $row_moduleID . "</title>";
                 echo "<tr>";
 
                 echo "<td>" . $row_dateTime . "</td>";
                 echo "<td>" . $row_data . "  </td>";
-                echo "</tr>";
+                echo "</tr>";*/
+
+
+
+                echo "<div class='row'>";
+                echo "<div class='col-1 border border border-dark''>" . $row_moduleID . "</div>";
+                echo "<div class='col-1 border border border-dark''>" . $row_data . "</div>";
+                echo "<div class='col-2 border border border-dark''>" . $row_dateTime. "</div>";
+                echo "<div class='col-1 border border border-dark''>" . $row_id . "</div>";
+                echo "</div>";
+
+
 
             }
             $result = null;
         }
         ?>
-    </table>
-</div>
 
 
-</body>
-</html>
+
+
+
