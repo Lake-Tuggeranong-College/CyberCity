23d358623c788f8df9b9da206d08a069462c77b2
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-09-24 16:55:05 +1000

    init

---
+++ Module code/findMe/src/sensitiveInformation.h
@@ -0,0 +1,19 @@
+/*
+ * Contains any sensitive Infomration that you do not want published to Github.
+ * 
+ * The SSID and Password variables will need to be changed if you’re connecting to another Wireless Access Point (such as at home). * The SSID and Password variables will need to be changed if you’re connecting to another Wireless Access Point (such as at home).
+
+ * The `http_username` and `http_password` variables are used to authenticate when users are attempting to access secured pages.
+ * 
+ * Make sure this file is included in the .gitignore!
+ * 
+ */
+
+const char* host = "RMS";
+const char* ssid = "gogogadgetnbn";       // Wifi Network Name
+const char* password = "st@rw@rs";          // Wifi Password
+
+String serverName = "http://192.168.1.10/CyberCity/dataTransfer.php";
+String apiKeyValue = "XjuTHh";
+String sensorName = "FindMe";
+String sensorLocation = "FindMe";
