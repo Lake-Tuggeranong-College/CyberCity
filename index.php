536124d0d60d0bbd2795eb50d4cb6adeea36bb2e
Ryan Cather <49006682+RyanCather@users.noreply.github.com>
2023-12-04 11:54:18 +1100

    refactor

--- index.php
+++ index.php
@@ -4,23 +4,21 @@
 
 <title>Cyber City</title>
 <div style="text-align: center;">
-<h1 class='text-primary'>Welcome to our Cyber City</h1>
+    <h1 class='text-primary'>Welcome to our Cyber City</h1>
 
 
     <?php
 
     if (isset($_SESSION["username"])) {
         echo
-              "<h2 class='text'> Your logged in, you now may play the CyberCity CTF game</h2>"
-                    ;
+        "<h2 class='text'> Your logged in, you now may play the CyberCity CTF game</h2>";
 
-            ?>
+        ?>
         <?php
     } else {
         echo
 
-                    "<h2 class='text'> Please login or register to participate in the CyberCity CTF Challenge</h2>"
-                    ;
+        "<h2 class='text'> Please login or register to participate in the CyberCity CTF Challenge</h2>";
     }
     ?>
     <img src="images/MainImage.jpg" alt="" width="50%" height="50%">
