7178be59e0c04950ac56223d437392ca92238cf0
0628222 <0628222@schoolsnet.act.edu.au>
2024-02-07 15:15:35 +1100

    fixing

--- Module code/Windmeels/Final_Windmill/sensitiveInformation.h
+++ Module code/Windmeels/Final_Windmill/sensitiveInformation.h
@@ -1,6 +1,6 @@
 const char* host = "RMS";
 const char* ssid = "RoboRange";       // Wifi Network Name
-const char* password = "RoboRange";  // Wifi Password
+const char* password = "Password01";  // Wifi Password
 
 //const char* serverName = "http://192.168.1.106/espPost/post-esp-data.php";
 String serverName = "http://192.168.1.10/CyberCity/dataTransfer.php";
