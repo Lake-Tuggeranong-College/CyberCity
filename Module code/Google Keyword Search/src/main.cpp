8572dd3cfafdcc50d0c135fe70d47861771fa19c
Glovania <9937412@schoolsnet.act.edu.au>
2024-03-14 17:08:46 +1100

    Added functionality for ESP32 module used for 'Google Keyword Search' challenge.
    Blink test for ESP32 worked.
    
    I2C address scan to detect 16x2 LCD address worked.
    
    TODO: figure out why display text for the LCD screen not working even though the code is 100% gurantee works. This might be a hardware issue.

--- Module code/Google Keyword Search/src/main.cpp
+++ Module code/Google Keyword Search/src/main.cpp
@@ -1,41 +1,91 @@
+// include the library code:
 #include <Arduino.h>
+#include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+
+// set the LCD number of columns and rows
+int lcdColumns = 16;
+int lcdRows = 2;
+
+// set LCD address, number of columns and rows
+// if you don't know your display address, run an I2C scanner sketch
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
 
 // ESP32 Blink test.
 int led = LED_BUILTIN;
-// 1602 ICD Display test.
-LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
 
 void setup() {
   // Some boards work best if we also make a serial connection
-  Serial.begin(115200);
-  lcd.init(); // initialize the lcd
-  lcd.backlight();
+  Serial.begin(9600);
 
   // set LED to be an output pin
   pinMode(led, OUTPUT);
+
+  // initialize the lcd 
+  lcd.init();
+  // turn on LCD backlight     
+  lcd.backlight();
+
+// Process to detect I2C device address.
+
+  // Wire.begin();
+  // Serial.println("\nI2C Scanner");
 }
 
 void loop() {
-  // Say hi!
-  Serial.println("Hello!");
-  
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("LED is current on!");   // Output text on serial monitor to show it's on.
   delay(500);                // wait for a half second
+
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  Serial.println("LED is current off!");  // Output text on serial monitor to show it's off.
   delay(500);                // wait for a half second
 
-  lcd.clear();                 // clear display
-  lcd.setCursor(0, 0);         // move cursor to   (0, 0)
-  lcd.print("Arduino");        // print message at (0, 0)
-  lcd.setCursor(2, 1);         // move cursor to   (2, 1)
-  lcd.print("GetStarted.com"); // print message at (2, 1)
-  delay(2000);                 // display the above for two seconds
+  // set cursor to first column, first row
+  lcd.setCursor(0, 0);
+  // print message
+  lcd.print("Hello, World!");
+  delay(1000);
+  // clears the display to print new message
+  lcd.clear();
+  // set cursor to first column, second row
+  lcd.setCursor(0,1);
+  lcd.print("Hello, World!");
+  delay(1000);
+  lcd.clear(); 
 
-  lcd.clear();                  // clear display
-  lcd.setCursor(3, 0);          // move cursor to   (3, 0)
-  lcd.print("DIYables");        // print message at (3, 0)
-  lcd.setCursor(0, 1);          // move cursor to   (0, 1)
-  lcd.print("www.diyables.io"); // print message at (0, 1)
-  delay(2000);                  // display the above for two seconds
+// Code to find I2C device address.
+
+  /* 
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+  delay(5000); 
+  */
 }
