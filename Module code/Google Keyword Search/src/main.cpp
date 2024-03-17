b14007e99bffb097869b250edf17c5422ceb060f
Glovania <9937412@schoolsnet.act.edu.au>
2024-03-17 19:08:46 +1100

    Swapped from using LCD display to MiniTFT display.
    Temporary swap as the LCD is not working properly.
    
    Still prefer to use the LCD more but that can be done later on.
    
    Added some missing libraries from last commit.
    
    Although the display text is still not working (probably becasue the device itself has some problems with the screen), I got the input respone when clicking on each buttons and joysticks from the MiniTFT.

--- Module code/Google Keyword Search/src/main.cpp
+++ Module code/Google Keyword Search/src/main.cpp
@@ -1,91 +1,181 @@
 // include the library code:
 #include <Arduino.h>
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <Adafruit_ST7789.h>
+#include "Adafruit_miniTFTWing.h"
 
-// set the LCD number of columns and rows
-int lcdColumns = 16;
-int lcdRows = 2;
+Adafruit_miniTFTWing ss;
+#define TFT_RST    -1    // we use the seesaw for resetting to save a pin
 
-// set LCD address, number of columns and rows
-// if you don't know your display address, run an I2C scanner sketch
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+#ifdef ESP8266
+   #define TFT_CS   0
+   #define TFT_DC   15
+#elif defined(ESP32) && !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
+   #define TFT_CS   15
+   #define TFT_DC   33
+#elif defined(TEENSYDUINO)
+   #define TFT_DC   10
+   #define TFT_CS   4
+#elif defined(ARDUINO_STM32_FEATHER)
+   #define TFT_DC   PB4
+   #define TFT_CS   PA15
+#elif defined(ARDUINO_NRF52832_FEATHER)  /* BSP 0.6.5 and higher! */
+   #define TFT_DC   11
+   #define TFT_CS   31
+#elif defined(ARDUINO_MAX32620FTHR) || defined(ARDUINO_MAX32630FTHR)
+   #define TFT_DC   P5_4
+   #define TFT_CS   P5_3
+#else
+    // Anything else, defaults!
+   #define TFT_CS   5
+   #define TFT_DC   6
+#endif
 
-// ESP32 Blink test.
-int led = LED_BUILTIN;
+Adafruit_ST7789 tft_7789 = Adafruit_ST7789(TFT_CS,  TFT_DC, TFT_RST);
+Adafruit_ST7735 tft_7735 = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 
+// we'll assign it later
+Adafruit_ST77xx *tft = NULL;
+uint32_t version;
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // Some boards work best if we also make a serial connection
   Serial.begin(9600);
+  while (!Serial) {
+    delay(10);  // Wait until serial console is opened
+  }
+  
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
 
-  // set LED to be an output pin
-  pinMode(led, OUTPUT);
+  if (!ss.begin()) {
+    Serial.println("seesaw couldn't be found!");
+    while(1);
+  }
 
-  // initialize the lcd 
-  lcd.init();
-  // turn on LCD backlight     
-  lcd.backlight();
+  version = ((ss.getVersion() >> 16) & 0xFFFF);
+  Serial.print("Version: "); Serial.println(version);
+  if (version == 3322) {
+    Serial.println("Version 2 TFT FeatherWing found");
+    
+  } else {
+    Serial.println("Version 1 TFT FeatherWing found");
+  }
+  
+  ss.tftReset();   // reset the display
+  ss.setBacklight(TFTWING_BACKLIGHT_ON);  // turn off the backlight
 
-// Process to detect I2C device address.
+  if (version == 3322) {
+    tft_7789.init(135, 240);
+    tft = &tft_7789;
+  } else {
+    tft_7735.initR(INITR_MINI160x80);   // initialize a ST7735S chip, mini display
+    tft = &tft_7735;
+  }
+  tft->setRotation(1);
+  Serial.println("TFT initialized");
 
-  // Wire.begin();
-  // Serial.println("\nI2C Scanner");
+  tft->fillScreen(ST77XX_RED);
+  delay(100);
+  tft->fillScreen(ST77XX_GREEN);
+  delay(100);
+  tft->fillScreen(ST77XX_BLUE);
+  delay(100);
+  tft->fillScreen(ST77XX_BLACK);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  Serial.println("LED is current on!");   // Output text on serial monitor to show it's on.
-  delay(500);                // wait for a half second
+  delay(10);
 
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  Serial.println("LED is current off!");  // Output text on serial monitor to show it's off.
-  delay(500);                // wait for a half second
+  digitalWrite(13, HIGH);           // turn the LED on (HIGH is the voltage level)
+  Serial.println("LED is on");      // Debug check if the LED is on or not.
+  delay(1000);                      // wait for a second
 
-  // set cursor to first column, first row
-  lcd.setCursor(0, 0);
-  // print message
-  lcd.print("Hello, World!");
-  delay(1000);
-  // clears the display to print new message
-  lcd.clear();
-  // set cursor to first column, second row
-  lcd.setCursor(0,1);
-  lcd.print("Hello, World!");
-  delay(1000);
-  lcd.clear(); 
+  digitalWrite(13, LOW);            // turn the LED off by making the voltage LOW
+  Serial.println("LED is off");     // Debug check if the LED is off or not.
+  delay(1000);                      // wait for a second
 
-// Code to find I2C device address.
+  uint32_t buttons = ss.readButtons();
+  //Serial.println(buttons, BIN);
+  
+  uint16_t color;
 
-  /* 
-  byte error, address;
-  int nDevices;
-  Serial.println("Scanning...");
-  nDevices = 0;
-  for(address = 1; address < 127; address++ ) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,HEX);
-      nDevices++;
-    }
-    else if (error==4) {
-      Serial.print("Unknow error at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,HEX);
-    }    
+  color = ST77XX_BLACK;
+  if (! (buttons & TFTWING_BUTTON_LEFT)) {
+    Serial.println("LEFT");
+    color = ST77XX_WHITE;
   }
-  if (nDevices == 0) {
-    Serial.println("No I2C devices found\n");
+  if (version == 3322) {
+    tft->fillTriangle(200, 45, 200, 85, 220, 65, color);
+  } else {
+    tft->fillTriangle(150, 30, 150, 50, 160, 40, color);
+  } 
+
+  color = ST77XX_BLACK;
+  if (! (buttons & TFTWING_BUTTON_RIGHT)) {
+    Serial.println("RIGHT");
+    color = ST77XX_WHITE;
   }
-  else {
-    Serial.println("done\n");
+  if (version == 3322) {
+    tft->fillTriangle(120, 45, 120, 85, 100, 65, color);
+  } else {
+    tft->fillTriangle(120, 30, 120, 50, 110, 40, color);
+  } 
+
+  color = ST77XX_BLACK;
+  if (! (buttons & TFTWING_BUTTON_DOWN)) {
+    Serial.println("DOWN");
+    color = ST77XX_WHITE;
   }
-  delay(5000); 
-  */
+  if (version == 3322) {
+    tft->fillTriangle(140, 25, 180, 25, 160, 10, color);
+  } else {
+    tft->fillTriangle(125, 26, 145, 26, 135, 16, color);
+  }
+  
+  color = ST77XX_BLACK;
+  if (! (buttons & TFTWING_BUTTON_UP)) {
+    Serial.println("UP");
+    color = ST77XX_WHITE;
+  }
+  if (version == 3322) {
+    tft->fillTriangle(140, 100, 180, 100, 160, 120, color);
+  } else {
+    tft->fillTriangle(125, 53, 145, 53, 135, 63, color);
+  }
+  
+  color = ST77XX_BLACK;
+  if (! (buttons & TFTWING_BUTTON_A)) {
+    Serial.println("A");
+    color = ST7735_GREEN;
+  }
+  if (version == 3322) {
+    tft->fillCircle(40, 100, 20, color);
+  } else {
+    tft->fillCircle(30, 57, 10, color);
+  }
+
+  color = ST77XX_BLACK;
+  if (! (buttons & TFTWING_BUTTON_B)) {
+    Serial.println("B");
+    color = ST77XX_YELLOW;
+  }
+  if (version == 3322) {
+    tft->fillCircle(40, 30, 20, color);
+  } else {
+    tft->fillCircle(30, 18, 10, color);
+  }
+  
+  color = ST77XX_BLACK;
+  if (! (buttons & TFTWING_BUTTON_SELECT)) {
+    Serial.println("SELECT");
+    color = ST77XX_RED;
+  }
+  if (version == 3322) {
+    tft->fillCircle(160, 65, 20, color);
+  } else {
+    tft->fillCircle(80, 40, 7, color);
+  }
 }
