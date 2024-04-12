8bdb8f13d23016da78ccaeb694f61bee887e4fba
0257638 <129924136+0257638@users.noreply.github.com>
2024-04-12 11:54:20 +1000

    updated sensitive info

--- Module code/windmills/src/sensitiveInformation.h
+++ Module code/windmills/src/sensitiveInformation.h
@@ -2,7 +2,7 @@ const char* host = "RMS";
 const char* ssid = "CyberRange";       // Wifi Network Name
 const char* password = "CyberRange";  // Wifi Password
 
-  String serverName = "http://192.168.1.106/espPost/post-esp-data.php";
+  String serverName = "http://192.168.1.10/CyberCity/dataTransfer.php";
 //String serverName = "http://10.177.200.10/CyberCity/dataTransfer.php";
 
 String apiKeyValue = "lqJPAy";
