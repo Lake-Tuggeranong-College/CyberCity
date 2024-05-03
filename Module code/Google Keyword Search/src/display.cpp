b40ace0b1be22e23d6fffeeb16ce2c4b5d551ae0
Glovania <9937412@schoolsnet.act.edu.au>
2024-05-03 15:00:08 +1000

    'test.cpp' is worked.
    Adding codes from the actual sample codes.
    
    Changing the 'CS' and 'DC' pins to the correct one according to the physical module.
    
    Adding delays for any output features onto the module to prevent memory overload.
    
    Copy the worked test file into the main implementation file to start working on the actual output data onto the TFT display.

--- Module code/Google Keyword Search/src/display.cpp
+++ Module code/Google Keyword Search/src/display.cpp
@@ -1,18 +1,8 @@
-/***************************************************
-  This is a library for the Adafruit 0.96" Mini TFT Featherwing
-  
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
- ****************************************************/
-
 // Core libraries.
 #include <Arduino.h>
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <Adafruit_ST7789.h>
 #include "Adafruit_miniTFTWing.h"
 
 // Custom Libraries
@@ -24,279 +14,80 @@ Adafruit_miniTFTWing ss;
 #define TFT_RST    -1    // we use the seesaw for resetting to save a pin
 
 #if defined(ESP32) && !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
-   #define TFT_CS   15
-   #define TFT_DC   33
+  #define TFT_CS   14
+  #define TFT_DC   32
 #endif
 
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+Adafruit_ST7789 tft_7789 = Adafruit_ST7789(TFT_CS,  TFT_DC, TFT_RST);
+Adafruit_ST7735 tft_7735 = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
-float p = 3.1415926;
+// we'll assign it later
+Adafruit_ST77xx *tft = NULL;
+uint32_t version;
 
-void testlines(uint16_t color) {
-  tft.fillScreen(ST77XX_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(0, 0, x, tft.height()-1, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(0, 0, tft.width()-1, y, color);
-  }
-
-  tft.fillScreen(ST77XX_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(tft.width()-1, 0, 0, y, color);
-  }
-
-  tft.fillScreen(ST77XX_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(0, tft.height()-1, x, 0, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
-  }
-
-  tft.fillScreen(ST77XX_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
-  }
-}
-
-void testdrawtext(const char *text, uint16_t color) {
-  tft.setCursor(0, 0);
-  tft.setTextColor(color);
-  tft.setTextWrap(true);
-  tft.print(text);
-}
-
-void testfastlines(uint16_t color1, uint16_t color2) {
-  tft.fillScreen(ST77XX_BLACK);
-  for (int16_t y=0; y < tft.height(); y+=5) {
-    tft.drawFastHLine(0, y, tft.width(), color1);
-  }
-  for (int16_t x=0; x < tft.width(); x+=5) {
-    tft.drawFastVLine(x, 0, tft.height(), color2);
-  }
-}
-
-void testdrawrects(uint16_t color) {
-  tft.fillScreen(ST77XX_BLACK);
-  for (int16_t x=0; x < tft.height(); x+=6) {
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);
-  }
-}
-
-void testfillrects(uint16_t color1, uint16_t color2) {
-  tft.fillScreen(ST77XX_BLACK);
-  for (int16_t x=tft.height()-1; x > 6; x-=6) {
-    tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);
-  }
-}
-
-void testfillcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=radius; x < tft.width(); x+=radius*2) {
-    for (int16_t y=radius; y < tft.height(); y+=radius*2) {
-      tft.fillCircle(x, y, radius, color);
-    }
-  }
-}
-
-void testdrawcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {
-    for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {
-      tft.drawCircle(x, y, radius, color);
-    }
-  }
-}
-
-void testtriangles() {
-  tft.fillScreen(ST77XX_BLACK);
-  uint16_t color = 0xF800;
-  int t;
-  int w = tft.width()/2;
-  int x = tft.height()-1;
-  int y = 0;
-  int z = tft.width();
-  for(t = 0 ; t <= 15; t++) {
-    tft.drawTriangle(w, y, y, x, z, x, color);
-    x-=4;
-    y+=4;
-    z-=4;
-    color+=100;
-  }
-}
-
-void testroundrects() {
-  tft.fillScreen(ST77XX_BLACK);
-  uint16_t color = 100;
-  int x = 0;
-  int y = 0;
-  int w = tft.width()-2;
-  int h = tft.height()-2;
-  while ((w > 10) && (h > 10)) {
-    tft.drawRoundRect(x, y, w, h, 5, color);
-    x+=2;
-    y+=3;
-    w-=4;
-    h-=6;
-    color+=1100;
-  }
-}
-
-void tftPrintTest() {
-  tft.setTextWrap(false);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setCursor(0, 0);
-  tft.setTextColor(ST77XX_RED);
-  tft.setTextSize(1);
-  tft.println("Hello World!");
-  tft.setTextColor(ST77XX_YELLOW);
-  tft.setTextSize(2);
-  tft.println("Hello World!");
-  tft.setTextColor(ST77XX_GREEN);
-  tft.setTextSize(3);
-  tft.println("Hello World!");
-  tft.setTextColor(ST77XX_BLUE);
-  tft.setTextSize(4);
-  tft.print(1234.567);
-  delay(1500);
-  tft.setCursor(0, 0);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(0);
-  tft.println("Hello World!");
-  tft.setTextSize(1);
-  tft.setTextColor(ST77XX_GREEN);
-  tft.print(p, 6);
-  tft.println(" Want pi?");
-  tft.println(" ");
-  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
-  tft.println(" Print HEX!");
-  tft.println(" ");
-  tft.setTextColor(ST77XX_WHITE);
-  tft.println("Sketch has been");
-  tft.println("running for: ");
-  tft.setTextColor(ST77XX_MAGENTA);
-  tft.print(millis() / 1000);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.print(" seconds.");
-}
-
-void mediabuttons() {
-  // play
-  tft.fillScreen(ST77XX_BLACK);
-  tft.fillRoundRect(0, 10, 78, 60, 8, ST77XX_WHITE);
-  tft.fillTriangle(17, 20, 17, 60, 65, 40, ST77XX_RED);
-  delay(500);
-  // pause
-  tft.fillRoundRect(82, 10, 78, 60, 8, ST77XX_WHITE);
-  tft.fillRoundRect(96, 18, 20, 45, 5, ST77XX_GREEN);
-  tft.fillRoundRect(127, 18, 20, 45, 5, ST77XX_GREEN);
-  delay(500);
-  // play color
-  tft.fillTriangle(17, 20, 17, 60, 65, 40, ST77XX_BLUE);
-  delay(50);
-  // pause color
-  tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_RED);
-  tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_RED);
-  // play color
-  tft.fillTriangle(17, 20, 17, 60, 65, 40, ST77XX_GREEN);
-}
-
-void setup(void) {
+void setup() {
   Serial.begin(9600);
   while (!Serial) {
-    delay(10);  // Wait until serial console is opened
+    delay(10); // Wait until serial console is opened.
   }
-  
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
-
-  Serial.print("Hello! ST77xx TFT Test");
   
   if (!ss.begin()) {
-    Serial.println("seesaw init error!");
+    Serial.println("seesaw not found!");
     while(1);
+  } else {
+    Serial.println("seesaw found!");
   }
-  else {
-    Serial.println("seesaw started");
+
+  version = ((ss.getVersion() >> 16) & 0xFFFF);
+  Serial.print("Version: "); Serial.println(version);
+  if (version == 3322) {
+    Serial.println("Version 2 TFT FeatherWing found");  
+  } else {
+    Serial.println("Version 1 TFT FeatherWing found");
   }
 
-  ss.tftReset();
-  ss.setBacklight(0x0); //set the backlight fully on
+  Serial.println("Reset module to the latest code...");
+  ss.tftReset();   // reset the display
+  Serial.println("Backlight is tunred off.");
+  ss.setBacklight(0x0000);  // turn off the backlight
 
-  // Use this initializer (uncomment) if you're using a 0.96" 180x60 TFT
-  tft.initR(INITR_MINI160x80);   // initialize a ST7735S chip, mini display
+  if (version == 3322) {
+    tft_7789.init(135, 240);
+    tft = &tft_7789;
+  } else {
+    tft_7735.initR(INITR_MINI160x80);   // initialize a ST7735S chip, mini display
+    tft = &tft_7735;
+  }
 
-  tft.setRotation(3);
-    
-  Serial.println("Initialized");
+  tft->setRotation(1);
+  Serial.println("TFT initialized");
 
-  uint16_t time = millis();
-  tft.fillScreen(ST77XX_BLACK);
-  time = millis() - time;
+  // tft->fillScreen(ST77XX_RED);
+  // delay(100);
+  // tft->fillScreen(ST77XX_GREEN);
+  // delay(100);
+  // tft->fillScreen(ST77XX_BLUE);
+  // delay(100);
+  // tft->fillScreen(ST77XX_BLACK);
 
-  Serial.println(time, DEC);
-  delay(500);
-
-  // large block of text
-  tft.fillScreen(ST77XX_BLACK);
-  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere.", ST77XX_WHITE);
+  Serial.println("Filling screen...");
+  tft->fillScreen(ST77XX_BLACK);
   delay(1000);
-
-  // tft print function!
-  tftPrintTest();
+  Serial.println("Setting text color...");
+  tft->setTextColor(ST77XX_WHITE, ST77XX_BLACK);
   delay(1000);
-
-  // a single pixel
-  tft.drawPixel(tft.width()/2, tft.height()/2, ST77XX_GREEN);
-  delay(500);
-
-  // line draw test
-  testlines(ST77XX_YELLOW);
-  delay(500);
-
-  // optimized lines
-  testfastlines(ST77XX_RED, ST77XX_BLUE);
-  delay(500);
-
-  testdrawrects(ST77XX_GREEN);
-  delay(500);
-
-  testfillrects(ST77XX_YELLOW, ST77XX_MAGENTA);
-  delay(500);
-
-  tft.fillScreen(ST77XX_BLACK);
-  testfillcircles(10, ST77XX_BLUE);
-  testdrawcircles(10, ST77XX_WHITE);
-  delay(500);
-
-  testroundrects();
-  delay(500);
-
-  testtriangles();
-  delay(500);
-
-  mediabuttons();
-  delay(500);
-
-  Serial.println("done");
+  Serial.println("Setting text size...");
+  tft->setTextSize(1);
   delay(1000);
+  Serial.println("Setting cursor position...");
+  tft->setCursor(0, 0);
+  delay(1000);
+  Serial.println("Printing to display...");
+  tft->print("Hello World");
 }
 
 void loop() {
-  digitalWrite(13, HIGH);           // turn the LED on (HIGH is the voltage level)
-  Serial.println("LED is on");      // Debug check if the LED is on or not.
-  delay(1000);                      // wait for a second
-
-  digitalWrite(13, LOW);            // turn the LED off by making the voltage LOW
-  Serial.println("LED is off");     // Debug check if the LED is off or not.
-  delay(1000);                      // wait for a second
+  delay(10);
 
   uint32_t buttons = ss.readButtons();
   //Serial.println(buttons, BIN);
@@ -305,42 +96,85 @@ void loop() {
 
   color = ST77XX_BLACK;
   if (! (buttons & TFTWING_BUTTON_LEFT)) {
+    delay(1000);
     Serial.println("LEFT");
     color = ST77XX_WHITE;
   }
+  if (version == 3322) {
+    tft->fillTriangle(200, 45, 200, 85, 220, 65, color);
+  } else {
+    tft->fillTriangle(150, 30, 150, 50, 160, 40, color);
+  } 
 
+  color = ST77XX_BLACK;
   if (! (buttons & TFTWING_BUTTON_RIGHT)) {
+    delay(1000);
     Serial.println("RIGHT");
     color = ST77XX_WHITE;
   }
+  if (version == 3322) {
+    tft->fillTriangle(120, 45, 120, 85, 100, 65, color);
+  } else {
+    tft->fillTriangle(120, 30, 120, 50, 110, 40, color);
+  } 
 
+  color = ST77XX_BLACK;
   if (! (buttons & TFTWING_BUTTON_DOWN)) {
+    delay(1000);
     Serial.println("DOWN");
     color = ST77XX_WHITE;
   }
-
+  if (version == 3322) {
+    tft->fillTriangle(140, 25, 180, 25, 160, 10, color);
+  } else {
+    tft->fillTriangle(125, 26, 145, 26, 135, 16, color);
+  }
+  
+  color = ST77XX_BLACK;
   if (! (buttons & TFTWING_BUTTON_UP)) {
+    delay(1000);
     Serial.println("UP");
     color = ST77XX_WHITE;
   }
-
+  if (version == 3322) {
+    tft->fillTriangle(140, 100, 180, 100, 160, 120, color);
+  } else {
+    tft->fillTriangle(125, 53, 145, 53, 135, 63, color);
+  }
+  
+  color = ST77XX_BLACK;
   if (! (buttons & TFTWING_BUTTON_A)) {
+    delay(1000);
     Serial.println("A");
     color = ST7735_GREEN;
   }
+  if (version == 3322) {
+    tft->fillCircle(40, 100, 20, color);
+  } else {
+    tft->fillCircle(30, 57, 10, color);
+  }
 
+  color = ST77XX_BLACK;
   if (! (buttons & TFTWING_BUTTON_B)) {
+    delay(1000);
     Serial.println("B");
     color = ST77XX_YELLOW;
   }
-
+  if (version == 3322) {
+    tft->fillCircle(40, 30, 20, color);
+  } else {
+    tft->fillCircle(30, 18, 10, color);
+  }
+  
+  color = ST77XX_BLACK;
   if (! (buttons & TFTWING_BUTTON_SELECT)) {
+    delay(1000);
     Serial.println("SELECT");
     color = ST77XX_RED;
   }
-
-  tft.invertDisplay(true);
-  delay(500);
-  tft.invertDisplay(false);
-  delay(500);
+  if (version == 3322) {
+    tft->fillCircle(160, 65, 20, color);
+  } else {
+    tft->fillCircle(80, 40, 7, color);
+  }
 }
