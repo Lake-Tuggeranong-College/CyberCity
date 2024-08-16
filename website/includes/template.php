404df5b8fd1b3aa2de491d86274b1e1b08a1f9fd
Nate-jon <124745330+Nate-jon@users.noreply.github.com>
2024-08-16 11:26:32 +1000

    fixed table css bug

--- website/includes/template.php
+++ website/includes/template.php
@@ -23,9 +23,9 @@ define('ADMIN_ACCESS_LEVEL', 2);
     <!-- Bootstrap CSS & Custom CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="<?= BASE_URL; ?>assets/css/styles.css">
-    <link rel="stylesheet" type="text/css" href="<?= BASE_URL; ?>assets/css/divContainer.css">
-    <link rel="stylesheet" type="text/css" href="<?= BASE_URL; ?>assets/css/moduleList.css">
+    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/css/styles.css">
+<!--    <link rel="stylesheet" href="--><?php //= BASE_URL; ?><!--assets/css/divContainer.css">-->
+    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/css/moduleList.css">
 </head>
 
 <body>
