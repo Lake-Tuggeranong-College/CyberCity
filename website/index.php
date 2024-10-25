aa9cc19f08063db92acff85a1b4fb3584ad06c43
Nate-jon <124745330+Nate-jon@users.noreply.github.com>
2024-10-25 11:52:34 +1100

    Update index.php

--- website/index.php
+++ website/index.php
@@ -35,55 +35,27 @@
 </div>
 
 
-<!--<div class = "wideBox">-->
-<!--    <div class ="subBoxWhite">-->
-<!--        <div class = "title" >-->
-<!---->
-<!--            <h1 style ="color: cornflowerblue">Learn</h1>-->
-<!--            <h2>The basics of Cyber</h2>-->
-<!---->
-<!--        </div>-->
-<!--    </div>-->
-<!---->
-<!--    <div class ="subBoxWhite">-->
-<!--        <div class = "title" >-->
-<!---->
-<!--            <h1 style ="color: lightgreen">Thrive</h1>-->
-<!--            <h2>Among us</h2>-->
-<!---->
-<!--        </div>-->
-<!--    </div>-->
-<!---->
-<!--    <div class ="subBoxWhite">-->
-<!--        <div class = "title" >-->
-<!---->
-<!--            <h1 style ="color: yellow">Connect</h1>-->
-<!--            <h2>Literally.</h2>-->
-<!---->
-<!--        </div>-->
-<!--    </div>-->
-<!---->
-<!---->
-<!--</div>-->
-
-
-<div class = "WideBox">
+<div class = "wideBox">
     <div class ="subBoxWhite">
         <div class = "title" >
+            <h2>Beginnings</h2>
+            <p>In 1850 a rural town was created, referred to as Latafa. This town was a logging town bringing in great riches for those who controlled it. During its earlier years, the town was a hot spot for illegal testing as it was far from any other towns. During the Red Tuesday bushfires in 1898, the town was consumed by a blazing inferno destroying the town. Later on, the town was rebuilt and the surrounding forest continued to grow. The people started to live off the land requiring less outside help leading to isolation, eventually leading to the erasure of the town off of the current day maps, to this day the location remains unknown and lost to time.    </p>
 
-            <h3>Rebels this is a call to action we have caught wind that the organisation TBW(The Black Watchman) has created a Super virus in Lab 404. It is your mission to assist us in putting an end to this sinister scheme, we want to cause mayhem to their system as the advanced hackers can take down the incubation pod to the virus, exterminating it by the roots.</h3>
+        </div>
+    </div>
+
+    <div class ="subBoxWhite">
+        <div class = "title" >
+            <h2>currently</h2>
+            <p>Oak-Crack is the remains of the town, the forest creating a natural barrier for which the TBW can hide. We at the LTC have found that the TBW is currently cultivating a super-virus in a French Bio-Lab by the name of Lab 404 deep underground.</p>
 
         </div>
     </div>
 
 
-    <div class ="subBoxWhite">
-        <div class = "title" >
 
-            <h3>We have sent you a list of operations we've found information on, please use the information given to cause havoc to TBW feel free to use the guide to help you.</h3>
-            <h4>Sincerely hacker-x.</h4>
-
-        </div>
-    </div>
 
 </div>
+
+
+
