3ebb830ce1b2d1fc5ad5b16d31906cd6e7653d99
Trixelit <36838998+Trixelit@users.noreply.github.com>
2024-11-04 11:30:01 +1100

    Complete docker implementation

---
+++ website/pages/challenges/docker/stopContainer.php
@@ -0,0 +1,22 @@
+<?php
+require('../../../includes/config.php');
+/**@var $conn*/
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    /**
+    echo "<pre>";
+    print_r($_POST); // or use var_dump($_POST);
+    echo "</pre>";
+    exit;
+     */
+    $dChallengeID = $_POST['dChallengeID'] ?? null;
+    $userID = $_POST['userID'] ?? null;
+
+    if (!$dChallengeID || !$userID) {
+        die("Error: Missing data for dChallengeID or userID.");
+    }
+    $current_date = date('Y-m-d H:i:s', 0);
+    echo $current_date;
+    $dockerContainerInsert = $conn->query("UPDATE IGNORE docker_containers SET timeInitialised = '$current_date' WHERE userID = '$userID' AND challengeID = '$dChallengeID'");
+}
+?>
