2a6905c5c28f26121e60d9c4ac140769859630ee
FinlayMcK <2681543@schoolsnet.act.edu.au>
2025-09-16 08:56:07 +1000

    Updated Timer

--- website/assets/30 sec timer.sh
+++ website/assets/30 sec timer.sh
@@ -8,9 +8,7 @@ DB_USER='CyberCity'
 DB_PASS='CyberCity'
 DB_HOST='10.177.202.196'
 
-NEW_VALUE='0'
-
-SQL="UPDATE Challenges SET moduleValue = 'NEW_VALUE';"
+SQL="UPDATE Challenges SET moduleValue = 0;"
 echo changed
 
-mysql -u "$DB_USER" -p "$DB_PASS" -h "$DB_HOST" "$DB_NAME" -e "$SQL"
+mysql -u$DB_USER -p$DB_PASS -h$DB_HOST -D$DB_NAME -e "$SQL"
